fb2aeb1fb6bbf39252c11ba8ba5d71cb55d66f59
Iskander Khaziev <gvalmon@gmail.com>
2023-10-17 22:20:11 +0000

    added specs for import_all method in package importer

--- spec/mono_repo_deps/package/importer_spec.rb
+++ spec/mono_repo_deps/package/importer_spec.rb
@@ -9,34 +9,24 @@ RSpec.describe MonoRepoDeps::Package::Importer do
     expect(Object.const_defined?(:OrdersApp)).to be true
   end
 
-  def expect_importing(package, imported_packages, entrypoints = [])
+  def expect_importing(package, imported_packages, entrypoints = [], only = nil)
     expect(subject).to receive(:import_dependency!).with(
-      {name: package, only: nil, skip: nil}, imported_packages, entrypoints, :test 
+      {name: package, only: only, skip: nil}, imported_packages, entrypoints, :test 
     ).and_call_original
     (imported_packages + [package]).uniq
   end
 
-  xit "will import dependencies in the right order" do
+  it "will #import dependencies in the right order" do
     imported_packages = []
     imported_packages = expect_importing(:orders_app, imported_packages)
     imported_packages = expect_importing(:orders_core, imported_packages)
     imported_packages = expect_importing(:orders_datasets, imported_packages)
     imported_packages = expect_importing(:db_connection, imported_packages)
     imported_packages = expect_importing(:test_utils, imported_packages)
-    imported_packages = expect_importing(:test_utils, imported_packages, [
-      a_string_matching("test_utils.rb"), 
-      a_string_matching("db_connection.rb")
-    ])
     imported_packages = expect_importing(:paypal_client, imported_packages, [
       a_string_matching("test_utils.rb"), 
       a_string_matching("db_connection.rb"),
       a_string_matching("orders_datasets.rb")
-    ])
-    imported_packages = expect_importing(:test_utils, imported_packages, [
-      a_string_matching("test_utils.rb"), 
-      a_string_matching("db_connection.rb"),
-      a_string_matching("orders_datasets.rb"),
-      a_string_matching("paypal_client.rb")
     ])
     imported_packages = expect_importing(:storefront_core, imported_packages, [
       a_string_matching("test_utils.rb"), 
@@ -49,71 +39,118 @@ RSpec.describe MonoRepoDeps::Package::Importer do
       a_string_matching("db_connection.rb"),
       a_string_matching("orders_datasets.rb"),
       a_string_matching("paypal_client.rb")
-    ])
-    imported_packages = expect_importing(:db_connection, imported_packages, [
-      a_string_matching("test_utils.rb"), 
-      a_string_matching("db_connection.rb"),
-      a_string_matching("orders_datasets.rb"),
-      a_string_matching("paypal_client.rb")
-    ])
-    imported_packages = expect_importing(:test_utils, imported_packages, [
-      a_string_matching("test_utils.rb"), 
-      a_string_matching("db_connection.rb"),
-      a_string_matching("orders_datasets.rb"),
-      a_string_matching("paypal_client.rb")
-    ])
-    imported_packages = expect_importing(:test_utils, imported_packages, [
-      a_string_matching("test_utils.rb"), 
-      a_string_matching("db_connection.rb"),
-      a_string_matching("orders_datasets.rb"),
-      a_string_matching("paypal_client.rb"),
-      a_string_matching("storefront_datasets.rb")
-    ])
-    imported_packages = expect_importing(:test_utils, imported_packages, [
-      a_string_matching("test_utils.rb"), 
-      a_string_matching("db_connection.rb"),
-      a_string_matching("orders_datasets.rb"),
-      a_string_matching("paypal_client.rb"),
-      a_string_matching("storefront_datasets.rb"),
-      a_string_matching("storefront_core.rb"),
-      a_string_matching("orders_core.rb")
-    ])
-    imported_packages = expect_importing(:storefront_core, imported_packages, [
-      a_string_matching("test_utils.rb"), 
-      a_string_matching("db_connection.rb"),
-      a_string_matching("orders_datasets.rb"),
-      a_string_matching("paypal_client.rb"),
-      a_string_matching("storefront_datasets.rb"),
-      a_string_matching("storefront_core.rb"),
-      a_string_matching("orders_core.rb")
     ])
     subject.call('orders_app', env: :test)
   end
 
-  it "will import dependencies in the right order" do
+  it "will #import_all dependencies in the right order" do
     imported_packages = []
-    imported_packages = expect_importing(:orders_app, imported_packages)
-    imported_packages = expect_importing(:orders_core, imported_packages)
-    imported_packages = expect_importing(:orders_datasets, imported_packages)
-    imported_packages = expect_importing(:db_connection, imported_packages)
     imported_packages = expect_importing(:test_utils, imported_packages)
-    imported_packages = expect_importing(:paypal_client, imported_packages, [
-      a_string_matching("test_utils.rb"), 
+    imported_packages = expect_importing(:db_connection, imported_packages, [
+      a_string_matching("test_utils.rb")
+    ])
+    imported_packages = expect_importing(:orders_datasets, imported_packages, [
+      a_string_matching("test_utils.rb"),
       a_string_matching("db_connection.rb"),
-      a_string_matching("orders_datasets.rb")
+    ])
+    imported_packages = expect_importing(:paypal_client, imported_packages, [
+      a_string_matching("test_utils.rb"),
+      a_string_matching("db_connection.rb"),
+      a_string_matching("orders_datasets.rb"),
+    ])
+    imported_packages = expect_importing(:orders_core, imported_packages, [
+      a_string_matching("test_utils.rb"),
+      a_string_matching("db_connection.rb"),
+      a_string_matching("orders_datasets.rb"),
+      a_string_matching("paypal_client.rb"),
     ])
     imported_packages = expect_importing(:storefront_core, imported_packages, [
-      a_string_matching("test_utils.rb"), 
+      a_string_matching("test_utils.rb"),
       a_string_matching("db_connection.rb"),
       a_string_matching("orders_datasets.rb"),
-      a_string_matching("paypal_client.rb")
+      a_string_matching("paypal_client.rb"),
     ])
     imported_packages = expect_importing(:storefront_datasets, imported_packages, [
-      a_string_matching("test_utils.rb"), 
+      a_string_matching("test_utils.rb"),
       a_string_matching("db_connection.rb"),
       a_string_matching("orders_datasets.rb"),
-      a_string_matching("paypal_client.rb")
+      a_string_matching("paypal_client.rb"),
     ])
-    subject.call('orders_app', env: :test)
+    imported_packages = expect_importing(:cart_query_api, imported_packages, [
+      a_string_matching("test_utils.rb"),
+      a_string_matching("db_connection.rb"),
+      a_string_matching("orders_datasets.rb"),
+      a_string_matching("paypal_client.rb"),
+      a_string_matching("storefront_datasets.rb"),
+      a_string_matching("storefront_core.rb"),
+      a_string_matching("orders_core.rb"),
+    ])
+    imported_packages = expect_importing(:orders_app, imported_packages, [
+      a_string_matching("test_utils.rb"),
+      a_string_matching("db_connection.rb"),
+      a_string_matching("orders_datasets.rb"),
+      a_string_matching("paypal_client.rb"),
+      a_string_matching("storefront_datasets.rb"),
+      a_string_matching("storefront_core.rb"),
+      a_string_matching("orders_core.rb"),
+      a_string_matching("cart_query_api.rb"),
+    ])
+    imported_packages = expect_importing(:cart_core, imported_packages, [
+      a_string_matching("test_utils.rb"),
+      a_string_matching("db_connection.rb"),
+      a_string_matching("orders_datasets.rb"),
+      a_string_matching("paypal_client.rb"),
+      a_string_matching("storefront_datasets.rb"),
+      a_string_matching("storefront_core.rb"),
+      a_string_matching("orders_core.rb"),
+      a_string_matching("cart_query_api.rb"),
+      a_string_matching("orders_app.rb"),
+    ])
+    imported_packages = expect_importing(:cart_datasets, imported_packages, [
+      a_string_matching("test_utils.rb"),
+      a_string_matching("db_connection.rb"),
+      a_string_matching("orders_datasets.rb"),
+      a_string_matching("paypal_client.rb"),
+      a_string_matching("storefront_datasets.rb"),
+      a_string_matching("storefront_core.rb"),
+      a_string_matching("orders_core.rb"),
+      a_string_matching("cart_query_api.rb"),
+      a_string_matching("orders_app.rb")
+    ])
+    imported_packages = expect_importing(:orders_query_api, imported_packages, [
+      a_string_matching("test_utils.rb"),
+      a_string_matching("db_connection.rb"),
+      a_string_matching("orders_datasets.rb"),
+      a_string_matching("paypal_client.rb"),
+      a_string_matching("storefront_datasets.rb"),
+      a_string_matching("storefront_core.rb"),
+      a_string_matching("orders_core.rb"),
+      a_string_matching("cart_query_api.rb"),
+      a_string_matching("orders_app.rb"),
+      a_string_matching("cart_datasets.rb")
+    ], [:orders_datasets])
+    
+    # imported_packages = expect_importing(:orders_core, imported_packages)
+    # imported_packages = expect_importing(:orders_datasets, imported_packages)
+    # imported_packages = expect_importing(:db_connection, imported_packages)
+    # imported_packages = expect_importing(:test_utils, imported_packages)
+    # imported_packages = expect_importing(:paypal_client, imported_packages, [
+    #   a_string_matching("test_utils.rb"), 
+    #   a_string_matching("db_connection.rb"),
+    #   a_string_matching("orders_datasets.rb")
+    # ])
+    # imported_packages = expect_importing(:storefront_core, imported_packages, [
+    #   a_string_matching("test_utils.rb"), 
+    #   a_string_matching("db_connection.rb"),
+    #   a_string_matching("orders_datasets.rb"),
+    #   a_string_matching("paypal_client.rb")
+    # ])
+    # imported_packages = expect_importing(:storefront_datasets, imported_packages, [
+    #   a_string_matching("test_utils.rb"), 
+    #   a_string_matching("db_connection.rb"),
+    #   a_string_matching("orders_datasets.rb"),
+    #   a_string_matching("paypal_client.rb")
+    # ])
+    subject.import_all
   end
 end
